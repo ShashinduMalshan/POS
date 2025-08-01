bd47cca059d3db6849eaf58529870f3320e0b88e
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-08-02 01:18:14 +0530

    create Auth response dto

---
+++ src/main/java/lk/ijse/spring/propos/dto/AuthResponseDTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.propos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String accessToken;
+}
