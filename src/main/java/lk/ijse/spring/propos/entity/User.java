35528b89ac302bdcd7c542b0aac31ee2500f50cf
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-08-02 01:00:39 +0530

    create user entity

---
+++ src/main/java/lk/ijse/spring/propos/entity/User.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.propos.entity;
+
+public class User {
+    private Long id;
+    private String username;
+    private String emailAddress;
+    private String role;
+    private String password;
+}
