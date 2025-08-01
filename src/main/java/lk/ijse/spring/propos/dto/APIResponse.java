d4012f8968976ab6a95e3e26eb6957340a741a23
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-08-02 01:13:45 +0530

    create API response dto

---
+++ src/main/java/lk/ijse/spring/propos/dto/APIResponse.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring.propos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class APIResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
