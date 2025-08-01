91c0222e58ea128537d6e0cd59e60053a5316ae6
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-08-02 01:03:08 +0530

    update user entity

--- src/main/java/lk/ijse/spring/propos/entity/User.java
+++ src/main/java/lk/ijse/spring/propos/entity/User.java
@@ -1,9 +1,23 @@
 package lk.ijse.spring.propos.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String emailAddress;
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private Role role;
     private String password;
 }
