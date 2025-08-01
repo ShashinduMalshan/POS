9d9188b53c6725f3af3258987d9de667376d3448
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2025-08-02 00:44:46 +0530

    first commit

---
+++ src/main/java/lk/ijse/spring/propos/ProPosApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.propos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/spring/propos/ProPosApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.propos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
